232632b02d8347dc12acc6b027c1820454ca7b4f
romainlepro <tirelalangue98@laposte.net>
2021-11-26 02:19:22 +0100

    content

---
+++ saveFile.py
@@ -0,0 +1,11 @@
+#"a" for append
+from time import *
+f = open("demofile.txt", "a")
+f.write(str(time())+"\n")
+f.write("Now the file has more content!")
+f.write("\n")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile.txt", "r")
+print(f.read())
