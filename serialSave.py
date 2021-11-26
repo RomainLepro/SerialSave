2fd8deaa8bdcbba8740231a8defd8086c811ccfa
Romain <tirelalangue98@laposte.net>
2021-11-26 15:56:17 +0100

    serial write cheked on Windows

--- serialSave.py
+++ serialSave.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 import serial
-
-
-
+from time import *
 
 
 if __name__ == '__main__':
     
     import serial.tools.list_ports as port_list
+    
+    
+    f = open("serialSave.txt", "a")
+    f.write("#"+str(time())+"\n")
+    
     ports = list(port_list.comports())
     for p in ports:
         print (p)
@@ -18,6 +21,7 @@ if __name__ == '__main__':
     except:
         #for windows
         ser = serial.Serial(ports[0][0], 9600, timeout=1)
+        
     ser.reset_input_buffer()
     
     ser.write("start")
@@ -25,4 +29,5 @@ if __name__ == '__main__':
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
+            f.write(line)
             print(line)
