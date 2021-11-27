f5d24983623217f25acbff06683793b4b7d604df
romainlepro <tirelalangue98@laposte.net>
2021-11-27 13:15:58 +0100

    serial save checked on rasp pi

--- saveFile.py
+++ saveFile.py
@@ -2,10 +2,11 @@
 from time import *
 f = open("demofile.txt", "a")
 f.write(str(time())+"\n")
-f.write("Now the file has more content!")
+f.write("Now the file has more content durin the reboot!")
 f.write("\n")
 f.close()
 
 #open and read the file after the appending:
 f = open("demofile.txt", "r")
 print(f.read())
+f.close()
